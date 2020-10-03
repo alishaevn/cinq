3668f33adf9af1db02d6532d76398241d4826de2
Alisha Evans <alishaevn2@gmail.com>
2020-10-02 23:12:38 -0700

    set up a devise User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # http://localhost:3000/rails/info/routes
+  
+  # root to: 'home#index'
 end
